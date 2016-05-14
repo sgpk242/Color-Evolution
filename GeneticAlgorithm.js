4c354be1105308283cd87066f8fc1f72a9cc2d35
Sean Keenan <skeenan@nd.edu>
2016-05-14 15:44:01 -0500

    Update GeneticAlgorithm.js

--- GeneticAlgorithm.js
+++ GeneticAlgorithm.js
@@ -31,38 +31,6 @@ ctx.font = "18px Arial";
 var generation = 1;
 ctx.fillText("Generation " + generation, 450, 260);
 
-
-
-function crossover(arr) {
-    var replacements = new Array();
-    
-    for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
-            RGB = Math.floor(Math.random() * 5);
-            if (RGB === 1) {
-                replacements[i] = new component(30, 30, arr[i*2].r, arr[i*2+1].g, arr[i*2].b, 0, 0, "9px Arial");
-            } else if (RGB === 2) {
-                replacements[i] = new component(30, 30, arr[i*2+1].r, arr[i*2].g, arr[i*2+1].b, 0, 0, "9px Arial");
-            } else if (RGB === 3) {
-                replacements[i] = new component(30, 30, arr[i*2].r, arr[i*2].g, arr[i*2+1].b, 0, 0, "9px Arial");
-            } else {
-                replacements[i] = new component(30, 30, arr[i*2+1].r, arr[i*2+1].g, arr[i*2].b, 0, 0, "9px Arial");
-            }
-        }
-    }
-    
-    return replacements;
-}
- 
-function compare(a,b) {
-  if (a.fitness > b.fitness)
-    return -1;
-  else if (a.fitness < b.fitness)
-    return 1;
-  else 
-    return 0;
-}
-
 function start() {
     var interv = setInterval(function(){
                      compete();
